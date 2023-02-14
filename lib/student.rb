576f959f1c1f2ffab4d67da61e6448034420c065
¨kericho¨ <lvisrono90@gmail.com>
2023-02-14 15:02:47 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+    attr_accessor :first_name, :last_name
+    attr_accessor :knowledge
+
+    def initialize
+        @first_name = first_name
+        @last_name = last_name
+    end
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(name)
+        
+        @knowledge = name
+    end
 
 end
